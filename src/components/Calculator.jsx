83537eab43f9bfd4aeac6485d03b8d741cc6be3d
Mateo Keller <mateokellergms@gmail.com>
2022-09-14 11:17:20 -0300

    added rows. functionallity missing

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import Button from "./Button";
 import "../App.css";
+import Button from "./Button";
 
 function Calculator() {
   const [result, setResult] = useState("");
 
   const handleClick = (e) => {
-    const name = e.target;
     setResult(result.concat(e.target.name));
+    console.log(e.target.name);
+  };
+
+  const handleDigit = (e) => {
+    setResult(result.concat());
+    console.log(e.target.id);
   };
 
   const clear = (e) => {
@@ -17,8 +22,6 @@ function Calculator() {
   const backspace = (e) => {
     setResult(result.slice(0, -1));
   };
-
-  // return math calculation
 
   const calculate = () => {
     try {
@@ -30,46 +33,68 @@ function Calculator() {
   };
 
   return (
-    <div className="container">
-      <form>
-        <input type="text" value={result} />
-      </form>
-
-      <div className="keypad"></div>
-      <button className="highlight" onClick={clear} id="clear">
-        Clear
-      </button>
-      <button className="highlight" onClick={backspace} id="backspace">
-        C
-      </button>
-      <button className="highlight" name="/" onClick={handleClick}>
-        &divide;
-      </button>
-      <Button className="digits" clickedNum={handleClick} />
-      <Button className="digits" clickedNum={handleClick} />
-      <Button className="digits" clickedNum={handleClick} />
-      <button className="highlight" name="*" onClick={handleClick}>
-        &times;
-      </button>
-
-      <button className="highlight" name="-" onClick={handleClick}>
-        &ndash;
-      </button>
-
-      <button className="highlight" name="+" onClick={handleClick}>
-        +
-      </button>
-      <button name="0" onClick={handleClick}>
-        0
-      </button>
-      <button name="." onClick={handleClick}>
-        .
-      </button>
-      <button className="highlight" onClick={calculate} id="result">
-        =
-      </button>
+    <div>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button className="highlight" onClick={clear} id="clear">
+            Clear
+          </button>
+          <button className="highlight" onClick={backspace} id="backspace">
+            C
+          </button>
+          <button className="highlight" name="&divide;" onClick={handleClick}>
+            &divide;
+          </button>
+          {[7, 8, 9].map((value, index) => (
+            <Button
+              key={value}
+              id={value}
+              name={value.toString()}
+              value={value.toString()}
+              clickedNum={handleDigit}
+            />
+          ))}
+          {/* {addRow()} */}
+          <button className="highlight" name="x" onClick={handleClick}>
+            &times;
+          </button>
+          {[4, 5, 6].map((value) => (
+            <Button
+              key={value}
+              id={value}
+              name={value.toString()}
+              value={value.toString()}
+              onClick={handleClick}
+            />
+          ))}
+          <button className="highlight" name="-" onClick={handleClick}>
+            &ndash;
+          </button>
+          {[1, 2, 3].map((value) => (
+            <Button
+              key={value}
+              id={value}
+              name={value.toString()}
+              value={value.toString()}
+              onClick={handleClick}
+            />
+          ))}
+          <button className="highlight" name="+" onClick={handleClick}>
+            +
+          </button>
+          <Button name="0" clickedNum={handleClick} />
+          <button name="." onClick={handleClick}>
+            .
+          </button>
+          <button className="highlight" onClick={calculate} id="result">
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Calculator;
